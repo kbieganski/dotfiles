82a3834c1107a3873328fa3804dc387805fba73c
Krzysztof Biegański <krzysztof@biegan.ski>
2023-02-20 12:45:16 +0100

    Switch to `init.lua`

--- init.lua
+++ init.lua
@@ -1,32 +1,42 @@
-"""""""""""""""
-""" Plugins """
-"""""""""""""""
-call plug#begin(stdpath('data') . 'plugged')
-    " needed for other plugins
+---------------
+--- Plugins ---
+---------------
+local Plug = function(plugin, options)
+    local arg = "'" .. plugin .. "'"
+    if options then
+        arg = arg .. ', ' .. options
+    end
+    vim.cmd.Plug(arg)
+end
+
+vim.cmd.call "plug#begin(stdpath('data') . 'plugged')"
+    -- needed for other plugins
     Plug 'nvim-lua/plenary.nvim'
     Plug 'nvim-lua/popup.nvim'
 
-    " picker UI
+    -- picker UI
     Plug 'nvim-telescope/telescope.nvim'
     Plug 'nvim-telescope/telescope-file-browser.nvim'
-    Plug 'nvim-telescope/telescope-fzf-native.nvim', {'do': 'make'}
+    Plug('nvim-telescope/telescope-fzf-native.nvim', "{'do': 'make'}")
     Plug 'nvim-telescope/telescope-ui-select.nvim'
     Plug 'nvim-telescope/telescope-github.nvim'
+    Plug 'nvim-telescope/telescope-dap.nvim'
     Plug 'cljoly/telescope-repo.nvim'
     Plug 'crispgm/telescope-heading.nvim'
 
-    " syntax highlighting
-    Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+    -- syntax highlighting
+    Plug('nvim-treesitter/nvim-treesitter', "{'do': ':TSUpdate'}")
     Plug 'nvim-treesitter/nvim-treesitter-textobjects'
 
-    " LSP utils
+    -- LSP utils
     Plug 'neovim/nvim-lspconfig'
-    Plug 'https://git.sr.ht/~whynothugo/lsp_lines.nvim' " diagnostic lines
+    Plug 'https://git.sr.ht/~whynothugo/lsp_lines.nvim' -- diagnostic lines
     Plug 'simrat39/rust-tools.nvim'
-    Plug 'lukas-reineke/lsp-format.nvim' " auto format
-    Plug 'SmiteshP/nvim-navic' " breadcrumbs
+    Plug 'lukas-reineke/lsp-format.nvim' -- auto format
+    Plug 'SmiteshP/nvim-navic' -- breadcrumbs
+    Plug 'folke/neodev.nvim'
 
-    " completion
+    -- completion
     Plug 'onsails/lspkind-nvim'
     Plug 'hrsh7th/cmp-vsnip'
     Plug 'hrsh7th/vim-vsnip'
@@ -36,130 +46,145 @@ call plug#begin(stdpath('data') . 'plugged')
     Plug 'hrsh7th/cmp-nvim-lua'
     Plug 'hrsh7th/nvim-cmp'
 
-    " git
+    -- git
     Plug 'lewis6991/gitsigns.nvim'
-    Plug 'akinsho/git-conflict.nvim', { 'tag': 'v1.0.0' }
+    Plug('akinsho/git-conflict.nvim', "{ 'tag': 'v1.0.0' }")
 
-    " word/range highlighting
-    Plug 'NvChad/nvim-colorizer.lua' " hex color highlighter
-    Plug 'winston0410/cmd-parser.nvim' " required for range-highlight
-    Plug 'winston0410/range-highlight.nvim' " highlights range entered in cmdline
-    Plug 'folke/todo-comments.nvim' " higlight todo comments
-    Plug 'RRethy/vim-illuminate' " highlight word under cursor
+    -- word/range highlighting
+    Plug 'NvChad/nvim-colorizer.lua' -- hex color highlighter
+    Plug 'winston0410/cmd-parser.nvim' -- required for range-highlight
+    Plug 'winston0410/range-highlight.nvim' -- highlights range entered in cmdline
+    Plug 'folke/todo-comments.nvim' -- higlight todo comments
+    Plug 'RRethy/vim-illuminate' -- highlight word under cursor
 
-    " UI improvements
-    Plug 'nvim-lualine/lualine.nvim' " statusline
-    Plug 'kyazdani42/nvim-web-devicons' " statusline icons
-    Plug 'folke/which-key.nvim' " show key mapping hints
-    Plug 'stevearc/dressing.nvim' " better select/input menus
-    Plug 'rcarriga/nvim-notify' " better notifications
-    Plug 'lewis6991/hover.nvim' " better hover
+    -- UI improvements
+    Plug 'nvim-lualine/lualine.nvim' -- statusline
+    Plug 'kyazdani42/nvim-web-devicons' -- statusline icons
+    Plug 'folke/which-key.nvim' -- show key mapping hints
+    Plug 'stevearc/dressing.nvim' -- better select/input menus
+    Plug 'rcarriga/nvim-notify' -- better notifications
+    Plug 'lewis6991/hover.nvim' -- better hover
 
-    " theming
-    Plug 'projekt0n/github-nvim-theme' " github colors
+    -- theming
+    Plug 'projekt0n/github-nvim-theme' -- github colors
     Plug 'xiyaowong/nvim-transparent'
 
-    " other
-    Plug 'LnL7/vim-nix' " nix language support
-    Plug 'Saecki/crates.nvim' " cargo file support
-    Plug 'mcauley-penney/tidy.nvim' " trim whitespace
-    Plug 'tpope/vim-eunuch' " unix commands in vim
-    Plug 'mfussenegger/nvim-dap' " debugging
-    Plug 'NMAC427/guess-indent.nvim' " guess indentation from file
-call plug#end()
+    -- other
+    Plug 'LnL7/vim-nix' -- nix language support
+    Plug 'Saecki/crates.nvim' -- cargo file support
+    Plug 'mcauley-penney/tidy.nvim' -- trim whitespace
+    Plug 'tpope/vim-eunuch' -- unix commands in vim
+    Plug 'mfussenegger/nvim-dap' -- debugging
+    Plug 'rcarriga/nvim-dap-ui' -- debugging
+    Plug 'NMAC427/guess-indent.nvim' -- guess indentation from file
+vim.cmd.call "plug#end()"
 
-""""""""""""""""""""""""
-""" General settings """
-""""""""""""""""""""""""
-syntax on
-set number " enable line numbers
-set ignorecase " when searching
-set smartcase " don't ignore case if search string contains uppercase letters
-set nocompatible " disable vi compatibility
-set incsearch " incremental searching
-set visualbell " disable bleeping
-set expandtab " insert spaces with tab
-set tabstop=4 " width of tab
-set shiftwidth=4 " width of indent
-set ruler " cursor position in the status line
-set cursorline " highlight line with cursor
-set autoindent " apply indentation of the previous line
-set smartindent " indent based on syntax
-set nohlsearch " do not highlight all search matches
-set virtualedit=all " allow virtual editing
-set backspace=indent,eol,start " backspace anything in insert mode
-set mouse=a " mouse support
-set autochdir " change working dir to buffer dir
-set completeopt=menuone,noselect " required for nvim-cmp
-set timeoutlen=250 " mapping timeout (which-key shows up after it)
-set hidden " switch between buffers without saving
-set undofile " persistent undo
-set nobackup " disable backup
-set nowritebackup
-set scrolloff=10 " keep cursor 10 lines from screen edge
-set termguicolors " prevent warning about opacity changes
+------------------------
+--- General settings ---
+------------------------
+vim.o.number = true  -- enable line numbers
+vim.o.ignorecase = true  -- when searching
+vim.o.smartcase = true  -- don't ignore case if search string contains uppercase letters
+vim.o.compatible = false  -- disable vi compatibility
+vim.o.incsearch = true  -- incremental searching
+vim.o.visualbell = true  -- disable bleeping
+vim.o.expandtab = true  -- insert spaces with tab
+vim.o.tabstop = 4  -- width of tab
+vim.o.shiftwidth = 4  -- width of indent
+vim.o.ruler = true  -- cursor position in the status line
+vim.o.cursorline = true  -- highlight line with cursor
+vim.o.autoindent = true  -- apply indentation of the previous line
+vim.o.smartindent = true  -- indent based on syntax
+vim.o.hlsearch = false  -- do not highlight all search matches
+vim.o.virtualedit = 'all'  -- allow virtual editing
+vim.o.backspace = 'indent,eol,start'  -- backspace anything in insert mode
+vim.o.mouse = 'a'  -- mouse support
+vim.o.autochdir = true  -- change working dir to buffer dir
+vim.o.completeopt = 'menuone,noselect'  -- required for nvim-cmp
+vim.o.timeoutlen = 250  -- mapping timeout (which-key shows up after it)
+vim.o.hidden = true  -- switch between buffers without saving
+vim.o.undofile = true  -- persistent undo
+vim.o.backup = false  -- disable backup
+vim.o.writebackup = false
+vim.o.scrolloff = 10  -- keep cursor 10 lines from screen edge
+vim.o.termguicolors = true  -- prevent warning about opacity changes
 
-""""""""""""""""
-""" Mappings """
-""""""""""""""""
-" TODO use which-key instead
-let g:mapleader=';' " set leader key to ;
+vim.g.mapleader = ';'
+vim.g.maplocalleader = ';'
 
-" browse headings
-nnoremap ga :Telescope heading<CR>
+----------------
+--- Mappings ---
+----------------
 
-" move up/down on visual lines
-nnoremap <expr> j v:count ? 'j' : 'gj'
-nnoremap <expr> k v:count ? 'k' : 'gk'
+-- browse headings
+vim.cmd.nnoremap 'gh :Telescope heading<CR>'
 
-" window managment with the alt key
-nnoremap <M-q> <C-w>q
-nnoremap <M-c> <C-w>s
-nnoremap <M-v> <C-w>v
-nnoremap <M-h> <C-w>h
-nnoremap <M-j> <C-w>j
-nnoremap <M-k> <C-w>k
-nnoremap <M-l> <C-w>l
-nnoremap <M-S-h> <C-w><S-h>
-nnoremap <M-S-j> <C-w><S-j>
-nnoremap <M-S-k> <C-w><S-k>
-nnoremap <M-S-l> <C-w><S-l>
+-- use system clilpboard by default
+vim.cmd.nnoremap 'y "+y'
+vim.cmd.nnoremap 'p "+p'
+vim.cmd.nnoremap 'P "+P'
 
-" faster scrolling
-noremap J <C-e><C-e>
-noremap K <C-y><C-y>
+-- delete without yanking
+vim.cmd.nnoremap 'd "_d'
+vim.cmd.vnoremap 'd "_d'
 
-" move through jump history
-noremap H <C-o>
-noremap L <C-i>
+-- use system clilpboard by default, and
+-- replace currently selected text with default register
+-- without yanking it
+vim.cmd.vnoremap 'y "+y'
+vim.cmd.vnoremap 'p "_d"+P'
+vim.cmd.vnoremap 'P "_d"+P'
 
-" make < > shifts keep selection
-vnoremap < <gv
-vnoremap > >gv
+-- move up/down on visual lines
+vim.cmd.nnoremap "<expr> j v:count ? 'j' : 'gj'"
+vim.cmd.nnoremap "<expr> k v:count ? 'k' : 'gk'"
+
+-- window managment with the alt key
+vim.cmd.nnoremap '<M-q> <C-w>q'
+vim.cmd.nnoremap '<M-c> <C-w>s'
+vim.cmd.nnoremap '<M-v> <C-w>v'
+vim.cmd.nnoremap '<M-h> <C-w>h'
+vim.cmd.nnoremap '<M-j> <C-w>j'
+vim.cmd.nnoremap '<M-k> <C-w>k'
+vim.cmd.nnoremap '<M-l> <C-w>l'
+vim.cmd.nnoremap '<M-S-h> <C-w><S-h>'
+vim.cmd.nnoremap '<M-S-j> <C-w><S-j>'
+vim.cmd.nnoremap '<M-S-k> <C-w><S-k>'
+vim.cmd.nnoremap '<M-S-l> <C-w><S-l>'
+
+-- faster scrolling
+vim.cmd.noremap 'J <C-e><C-e>'
+vim.cmd.noremap 'K <C-y><C-y>'
+
+-- move through jump history
+vim.cmd.noremap 'H <C-o>'
+vim.cmd.noremap 'L <C-i>'
+
+-- make < > shifts keep selection
+vim.cmd.vnoremap '< <gv'
+vim.cmd.vnoremap '> >gv'
 
 
-"""""""""""""""""""
-""" Line length """
-"""""""""""""""""""
+-------------------
+--- Line length ---
+-------------------
 
-au BufRead,BufNewFile *.md setlocal textwidth=80
+vim.cmd.au 'BufRead,BufNewFile *.md setlocal textwidth=80'
+vim.cmd.colorscheme 'github_dark'  -- after lualine setup
 
-colorscheme github_dark " after lualine setup
+--------------------------
+--- Snippet navigation ---
+--------------------------
 
-"""""""""""""""""""""""""""
-""" Snippet navigation """"
-"""""""""""""""""""""""""""
+vim.cmd.imap "<expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'"
+vim.cmd.smap "<expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'"
+vim.cmd.imap "<expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'"
+vim.cmd.smap "<expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'"
+vim.cmd.imap "<expr> <M-l>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<M-l>'"
+vim.cmd.smap "<expr> <M-l>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<M-l>'"
+vim.cmd.imap "<expr> <M-h> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<M-h>'"
+vim.cmd.smap "<expr> <M-h> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<M-h>'"
 
-imap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
-smap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
-imap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
-smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
-imap <expr> <M-l>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<M-l>'
-smap <expr> <M-l>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<M-l>'
-imap <expr> <M-h> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<M-h>'
-smap <expr> <M-h> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<M-h>'
-
-lua <<EOF
 -----------------
 --- Which-key ---
 -----------------
@@ -178,7 +203,7 @@ wk.setup {
 -----------------
 --- Telescope ---
 -----------------
-local telescope = require 'telescope'
+telescope = require 'telescope'
 telescope.setup {
     defaults = {
         -- switch between horizontal/vertical layout based on window size
@@ -208,12 +233,14 @@ telescope.load_extension 'ui-select'
 telescope.load_extension 'gh'
 telescope.load_extension 'repo'
 telescope.load_extension 'heading'
+telescope.load_extension 'dap'
 
 telescope_builtin = require 'telescope.builtin'
+telescope_utils = require 'telescope.utils'
 
 wk.register({
         ['/'] = { telescope_builtin.current_buffer_fuzzy_find, "Find in current file" },
-        ['?'] = { function() telescope_builtin.find_files { hidden = true, follow = true } end, "Find in files" },
+        ['?'] = { telescope_builtin.live_grep, "Find in files" },
         b = { function() telescope_builtin.buffers{ignore_current_buffer=true, sort_mru=true} end, "Buffers" },
         d = { vim.diagnostic.open_float, "Show this diagnostic" },
         D = { telescope_builtin.diagnostics, "All diagnostics" },
@@ -296,18 +323,44 @@ function setup_lsp_common(client, bufnr)
     end
 end
 
+require("neodev").setup({
+  -- add any options here, or leave empty to use the default settings
+})
+
+-- then setup your lsp server as usual
+local lspconfig = require('lspconfig')
+
+
 local lspconfig = require 'lspconfig'
-for _, server in pairs { "clangd", "gopls", "hls", "pylsp", "tsserver" } do
+for _, server in pairs { "clangd", "gopls", "hls", "pylsp", "tsserver", "verible" } do
     lspconfig[server].setup {
         capabilities = capabilities,
         on_attach = function(client, bufnr)
-            if server ~= "clangd" then
+            if server ~= "clangd" and server ~= "verible" then
                 require 'lsp-format'.on_attach(client)
             end
             setup_lsp_common(client, bufnr)
         end,
     }
 end
+
+require("neodev").setup({
+  -- add any options here, or leave empty to use the default settings
+})
+
+-- then setup your lsp server as usual
+local lspconfig = require('lspconfig')
+
+-- example to setup lua_ls and enable call snippets
+lspconfig.lua_ls.setup({
+  settings = {
+    Lua = {
+      completion = {
+        callSnippet = "Replace"
+      }
+    }
+  }
+})
 
 -- Rust
 -- (this sets up rust-analyzer, so don't do it above)
@@ -385,10 +438,8 @@ require 'gitsigns'.setup {
     current_line_blame = true,
     on_attach = function(bufnr)
          wk.register({
-                g = {
-                    h = { ':Gitsigns next_hunk<CR>', "Next hunk" },
-                    H = { ':Gitsigns prev_hunk<CR>', "Previous hunk" },
-                },
+                [']h'] = { ':Gitsigns next_hunk<CR>', "Next hunk" },
+                ['[h'] = { ':Gitsigns prev_hunk<CR>', "Previous hunk" },
             })
         wk.register({
                 g = {
@@ -529,4 +580,3 @@ require 'dressing'.setup{
 require 'transparent'.setup { enable = true }
 require 'tidy'.setup()
 require 'guess-indent'.setup()
-EOF
